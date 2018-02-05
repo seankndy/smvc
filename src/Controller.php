9d4954278a2b12a505365179412eb68c2e59b1d5
Sean Kennedy <sean@kndy.net>
2018-02-05 08:41:39 -0700

    fix git screwups

--- src/Controller.php
+++ src/Controller.php
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-<?php
-namespace SeanKndy\SMVC;
-
-use Psr\Http\Message\ServerRequestInterface;
-use GuzzleHttp\Psr7\Response;
-
-class Controller
-{
-    protected $response;
-    protected $view;
-    protected $app;
-    protected $session;
-
-    public function __construct(Application $app) {
-        $this->app = $app;
-		$this->session = $this->app->getSession();
-        $this->response = new Response();
-        $this->view = new View($this->app, $this->response);
-    }
-
-    public function getRequest() {
-        return $this->app->getRequest();
-    }
-
-    public function redirectToRoute($name, array $args = []) {
-        $this->response = new Response(302);
-        $this->response = $this->response->withHeader('Location', $this->app->url($name, $args));
-        return $this->response;
-    }
-
-    // used to instantiate models.
-    /*
-    public function __get($var) {
-        if (isset($this->models[$var]))
-            return $this->models[$var];
-        $modelClass = $this->app->config('namespace') . '\\' . $var . 'Model';
-        if ($model = Util::instantiateModel($var, $this->db)) {
-            $this->models[$var] = $model;
-            return $this->models[$var];
-        }
-        return null;
-    }
-    */
-}
-=======
 <?php
 namespace SeanKndy\SMVC;
 
@@ -97,4 +51,3 @@ class Controller
         return null;
     }
 }
->>>>>>> b3073afc0b2ef2f249c071a7a00c7b1677579668
